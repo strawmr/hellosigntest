dd9462f46f829b56b5dfab416c5cd5b3c995f7ac
Mike Straw <mstrawjr@gmail.com>
2018-06-01 13:01:06 -0400

    updated model

--- app/models/hellosigntemplate.rb
+++ app/models/hellosigntemplate.rb
@@ -1,20 +1,15 @@
-# client = HelloSign::Client.new :api_key => '4fa79428f9b0384b3712d04f17d4601c30be9f5a2d19cf776465fc83e92e38d2'
-# client.create_embedded_signature_request_with_template(
-#     :test_mode => 1,
-#     :client_id => 'b283237461ddc11a6425aa0fe2cf262d',
-#     :template_id => 'b0ee832977d76cc3240364e0287ccfd1544bb454',
-#     :subject => 'My First embedded signature request with a reusable form',
-#     :message => 'Awesome, right?',
-#     :signers => [
-#         {
-#             :email_address => 'jack@example.com',
-#             :name => 'Jack',
-#             :role => 'Manager'
-#         },
-#         {
-#             :email_address => 'jill@example.com',
-#             :name => 'Jill',
-#             :role => 'Employee'
-#         }
-#     ]
-# )
+client = HelloSign::Client.new :api_key => '4fa79428f9b0384b3712d04f17d4601c30be9f5a2d19cf776465fc83e92e38d2'
+client.create_embedded_signature_request_with_template(
+    :test_mode => 1,
+    :client_id => 'b283237461ddc11a6425aa0fe2cf262d',
+    :template_id => '358837018a0894b3c334c2994f29b0baf967d1b1',
+    :subject => 'My First embedded signature request with a reusable form',
+    :message => 'Awesome, right?',
+    :signers => [
+        {
+            :email_address => 'mstrawjr@gmail.com',
+            :name => 'Mike',
+            :role => 'Signer'
+        }
+    ]
+)
