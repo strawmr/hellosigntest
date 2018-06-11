fcbe9f242064edafe7211a702b7170c889ff1c2a
Mike Straw <mstrawjr@gmail.com>
2018-06-11 13:32:11 -0400

    updated to work on localhost

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -18,15 +18,17 @@ end
 
 private
 def create_embedded_request(opts = {})
-HelloSign.create_embedded_signature_request(
+HelloSign.create_embedded_signature_request_with_template(
 test_mode: 1,
 client_id: 'b283237461ddc11a6425aa0fe2cf262d',
+template_id:'358837018a0894b3c334c2994f29b0baf967d1b1',
 subject: 'My first embedded signature request',
 message: 'Awesome, right?',
 signers: [
     {  email_address: opts[:email],
-       name: opts[:name]      }    ],
-files: ['test.pdf']  ) end
+       name: opts[:name],
+       role: 'Signer'     }    ] )
+end
 
 def get_sign_url(embedded_request)
   sign_id = get_first_signature_id(embedded_request)
